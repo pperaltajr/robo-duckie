bebe4f6e1acacf88fb921687fab8ec4b9f33e193
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-02-10 16:35:37 -0500

    updated

---
+++ packages/hw2_package/src/check_container.py
@@ -0,0 +1,2 @@
+#this is checking to see if it populates from outside of container to inside of repo container
+
--- packages/hw2_package/src/publisher_node.py
+++ packages/hw2_package/src/publisher_node.py
@@ -1,1 +1,25 @@
-Hello
+#!/usr/bin/env python3
+# Adapted from http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
+
+import rospy
+from std_msgs.msg import String
+
+class Talker:
+    def __init__(self):
+        self.pub = rospy.Publisher('chatter', String, queue_size=10)
+        
+    def talk(self):
+        hello_str = "hello world %s" % rospy.get_time()
+        #rospy.loginfo(hello_str)
+        self.pub.publish(hello_str)
+        
+if __name__ == '__main__':
+    try:
+        rospy.init_node('talker', anonymous=True)
+        t = Talker()
+        rate = rospy.Rate(1) #1hz
+        while not rospy.is_shutdown():
+            t.talk()
+            rate.sleep()
+    except rospy.ROSInterruptException:
+        pass
