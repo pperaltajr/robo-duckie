cd5b21e99173ec282841893b502c531813bc774f
root <pedro_peralta@student.uml.edu>
2021-02-12 04:23:20 +0000

    Final Commit hw2

--- packages/hw2_package/src/publisher_node.py
+++ packages/hw2_package/src/publisher_node.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
-# Adapted from http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
 
 import rospy
 from std_msgs.msg import Float32
 
 class Talker:
     def __init__(self):
-        self.pub = rospy.Publisher('publisher', Float32, queue_size=10)
+        self.pub = rospy.Publisher('/input', Float32, queue_size=10)
         
     def fibonacci(self):
-         # Program to display the Fibonacci sequence up to the n-th term
-        
-        nterms = 13
+        # Program to display the Fibonacci sequence up to the n-th term
+        nterms = 8
+       
         # first two terms  
         n1, n2, = 0, 1
         count = 0
        
-        while count < 10:
+        while count < nterms:
             print(n1)
             nth = n1 + n2
             # update values
             n1 = n2
             n2 = nth
             count += 1
+            rospy.loginfo(n1)
             self.pub.publish(n1)  
         
         
 if __name__ == '__main__':
     try:
-        rospy.init_node('input', anonymous=True)
+        rospy.init_node("publish_node", anonymous=True)
         t = Talker()
-        rate = rospy.Rate(1) #1hz
+        rate = rospy.Rate(.5) #0.5hz
         while not rospy.is_shutdown():
             t.fibonacci()
             rate.sleep()
--- packages/hw2_package/src/sub1_node.py
+++ packages/hw2_package/src/sub1_node.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
-# Adapted from http://wiki.ros.org/ROS/Tutorials/WritingPublisherSubscriber%28python%29
 
 import rospy
 from std_msgs.msg import Float32
+from mystery_package.msg import UnitsLabelled
 
 class Listener:
     def __init__(self):
-        rospy.Subscriber('output1', Float32, self.callback)
+        rospy.Subscriber('/output1', Float32, self.callback)
+        rospy.Subscriber('/output2', UnitsLabelled, self.callback2)
+   
         
     def callback(self, data):
         rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+      
+    def callback2(self, msg):
+        rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
+    
         
 if __name__ == '__main__':
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node('subsriber_node', anonymous=True)
     Listener()
     #spin() simply keeps python from exiting until this note is stopped
     
