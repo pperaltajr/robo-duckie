97174c915ad3ae9115aa6938801e3de68ee879c6
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-02-16 00:20:28 -0500

    feetgood

--- packages/hw2_package/src/subpub_node.py
+++ packages/hw2_package/src/subpub_node.py
@@ -5,18 +5,24 @@ from std_msgs.msg import Float32
 from mystery_package.msg import UnitsLabelled
 
 class Listener:
-    def __init__(self):
+    def __init__(self):        
+        self.total = 0
+        self.pub_msg = UnitsLabelled()
+        self.pub_msg.units = "Feet"
         rospy.Subscriber('/output2', UnitsLabelled, self.callback)
-        self.pub = rospy.Publisher('/input', Float32, queue_size=10)
+        self.pub_units = rospy.Publisher('/output3', UnitsLabelled, queue_size=10)
         
              
     def callback(self, msg):
-        rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
+        self.pub_msg.value = self.total
+        self.pub_units.publish(self.pub_msg)
+        
+        
+        #rospy.loginfo(rospy.get_caller_id() + "I heard %s", msg)
     
         
 if __name__ == '__main__':
     rospy.init_node('subpubnode', anonymous=True)
     Listener()
-    #spin() simply keeps python from exiting until this note is stopped
-    
+    #spin() simply keeps python from exiting until this note is stopped  
     rospy.spin()   
