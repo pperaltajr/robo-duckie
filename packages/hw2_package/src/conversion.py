df197cf4d9b9fd27e848c8ddfb8816219744e221
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-03-04 20:39:41 -0500

    hw4 almost complete

--- packages/hw2_package/src/conversion.py
+++ packages/hw2_package/src/conversion.py
@@ -6,30 +6,31 @@ from mystery_package.msg import UnitsLabelled
 
 class Listener:
     def __init__(self):
-        rospy.Subscriber('/output2', UnitsLabelled, self.callback2)
-        self.pub_units = rospy.Publisher('/output4', UnitsLabelled, queue_size=10)
+        rospy.Subscriber('/output2', UnitsLabelled, self.callback)
+        self.pub = rospy.Publisher('/output4', UnitsLabelled, queue_size=10)
         self.pub_msg = UnitsLabelled()
-        self.pub_msg.units = "Feet"
-        self.total = 0
+        self.pub_msg.units = "feet"
              
-    def callback2(self, msg):
+    def callback(self, msg):
+        
         if rospy.has_param("meters"):
             self.unit = rospy.get_param("meters")
-        else:
-            self.unit = "Feet"
+            datameters = msg.value
+            rospy.loginfo("%s %s", datameters, self.unit)
+            self.pub.publish(datameters, self.pub_msg.units)
             
-        # checks parameters and calculates based on units
-        if self.unit == "meters":
-            self.total = msg.value
-        elif self.unit == "Feet":
-            self.total = self.total*3.281
-        elif self.unit == "Smoots":
-            self.total = self.total*1.7018
-
-        self.pub_msg.value = self.total
-        self.pub_units.publish(self.pub_msg)
-        rospy.loginfo("Conversion output: %s", self.pub_msg.value)
-    
+        elif rospy.has_param("feet"):
+            self.unit = rospy.get_param("feet")
+            datafeet = msg.value * 3.2808
+            rospy.loginfo("%s %s", datafeet, self.unit)
+            self.pub.publish(datafeet, self.pub_msg.units)
+        
+        elif rospy.has_param("smoots"):
+            self.unit = rospy.get_param("smoots")
+            datasmoots = msg.value * .587613
+            rospy.loginfo("%s %s", datasmoots, self.unit)
+            self.pub.publish(datasmoots, self.pub_msg.units)
+            
         
 if __name__ == '__main__':
     rospy.init_node('conversion', anonymous=True)
