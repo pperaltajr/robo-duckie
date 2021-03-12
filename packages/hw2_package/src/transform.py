71b267d37f7b86332d647229206f706fcb4c6a38
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-03-12 01:47:09 -0500

    hw5launch

--- packages/hw2_package/src/transform.py
+++ packages/hw2_package/src/transform.py
@@ -9,26 +9,30 @@ class Transform:
 
     def __init__(self):
 
-        rospy.Subscriber('/output2', Float32, self.callback)
-        self.pub_units = rospy.Publisher('/robot', Float32, queue_size=10)
-        self.pub_units = rospy.Publisher('/world', Float32, queue_size=10)
-        self.pub_msg = UnitsLabelled()
-        self.pub_msg.units = "Feet"
-
+        rospy.Subscriber('/output2', Vector2D, self.callback)
+        self.pub_robot = rospy.Publisher('/robot', Vector2D, queue_size=10)
+        self.pub_world = rospy.Publisher('/world', Vector2D, queue_size=10)
+        self.values = Vector2D()
+        x = self.values.x
+        y = self.values.y
              
     def callback(self, msg):
+            
+        v = [[x],[y],[1]]
+        transform1 = numpy.matrix([[-1, 0, -1],[0, -1, 0],[0, 0, 1]])    
+        calc1 = transform * v
         
+        transform2 = numpy.matrix([[-.707, -.707, 10],[.707, -.707, 5],[0, 0, 1]])    
+        calc2 = transform * v
         
-   '''     self.total = msg.value
-        self.pub_msg.value = self.total * 3.281
-        self.pub_units.publish(self.pub_msg)
-        rospy.loginfo("Converting meters from fibonacci to feet: %s", self.pub_msg)
-   '''
+        self.pub_robot.publish(calc1)
+        self.pub_world.publish(calc2)
+        
         
 if __name__ == '__main__':
     rospy.init_node('transform', anonymous=True)
-    Listener()
+    Transform()
     
     #spin() simply keeps python from exiting until this note is stopped   
     rospy.spin()
-    
+   
