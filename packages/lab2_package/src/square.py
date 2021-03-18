481d1860dbdbe80de2f7a16f56cf99495471049a
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-03-17 23:37:13 -0400

    square

---
+++ packages/lab2_package/src/square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Float32
+from duckietown_msgs.msg import Twist2DStamped
+
+class Square:
+    def __init__(self):
+        self.pub = rospy.Publisher('car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
+             
+    def move(self,y,x): 
+        moveMsg = Twist2DStamped()
+        moveMsg.v = y
+        moveMsg.omega = x               
+        self.pub.publish(moveMsg)
+        		
+if __name__ == '__main__':
+    try:
+        rospy.init_node('square')
+        robot = Square()
+        rate = rospy.rate(10)
+        count = 0
+        for c in range(0,4):
+            for count in range(0,10):
+                robot.move(0,0)
+                rate.sleep()
+            for count in range(0,20):
+                robot.move(0.5,0)
+                rate.sleep()
+            for count in range(0,10):
+                robot.move(0,0)
+                rate.sleep()
+            for count in range(0,6):
+                robot.move(0,3)
+                rate.sleep()
+            robot.move(0,0)
+        robot.move(0,0)
+    except rospy.ROSInterruptException:
+        pass
