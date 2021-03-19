d4bd1617b196fb4cff898910ebf5ed55f1ba9f6a
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-03-19 01:19:44 -0400

    hw6update

--- packages/odometry_package/src/odometry.py
+++ packages/odometry_package/src/odometry.py
@@ -8,11 +8,15 @@ from duckietown_msgs.msg import Twist2DStamped
 class Odometry:
     def __init__(self):
         rospy.Subscriber('/dist_wheel', DistWheel, self.callback)
-        self.pub_units = rospy.Publisher('/pose', Pose2D, queue_size=10)
-
-             
+        self.pub = rospy.Publisher('/pose', Pose2D, queue_size=10)
+        self.pub_msg = DistWheel()
+        
     def callback(self, msg):
-        rospy.loginfo("Pose: %s", self.pub_msg)
+        x_value = msg.dist_wheel_left
+        y_value = msg.dist_wheel_right
+        
+        
+        self.pub.publish(.25, .25, .8)
     
         
 if __name__ == '__main__':
