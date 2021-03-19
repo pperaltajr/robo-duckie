0246dcc9c484b42794ec6dca5803f41666c81b1c
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-03-19 02:04:16 -0400

    hw6_so_close

--- packages/odometry_package/src/odometry.py
+++ packages/odometry_package/src/odometry.py
@@ -16,21 +16,26 @@ class Odometry:
         x = 0
         y = 0
         theta = 0
+        loop = 0
         
-        x_value = msg.dist_wheel_left
-        y_value = msg.dist_wheel_right
+        while (loop < 50):
+            x_value = msg.dist_wheel_left
+            y_value = msg.dist_wheel_right
         
-        s_delta = (x_value + y_value)/2
-        theta_delta = (x_value - y_value)/0.1
+            s_delta = (x_value + y_value)/2
+            theta_delta = (x_value - y_value)/0.1
         
-        x_delta = s_delta*cos(theta + theta_delta/2)
-        y_delta = s_delta*sin(theta + theta_delta/2)
+            x_delta = s_delta*cos(theta + theta_delta/2)
+            y_delta = s_delta*sin(theta + theta_delta/2)
         
-        x_prime = x + x_delta
-        y_prime = y + y_delta
-        theta_prime = theta + theta_delta      
+            x_prime = x + x_delta
+            y_prime = y + y_delta
+            theta_prime = theta + theta_delta    
+            self.pub.publish(x_prime, y_prime, theta_prime)
+              
+            loop += 1
+            
         
-        self.pub.publish(x_prime, y_prime, theta_prime)
     
         
 if __name__ == '__main__':
