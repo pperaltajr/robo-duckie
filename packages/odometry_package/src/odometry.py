d230efcb9f0473c7a0595eac7f0cb48f2088a4b7
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-03-19 01:35:36 -0400

    hw6_test

--- packages/odometry_package/src/odometry.py
+++ packages/odometry_package/src/odometry.py
@@ -2,6 +2,7 @@
 
 import rospy
 from std_msgs.msg import Float32
+from math import radians, sin, cos
 from odometry_hw.msg import DistWheel, Pose2D
 from duckietown_msgs.msg import Twist2DStamped
 
@@ -12,11 +13,24 @@ class Odometry:
         self.pub_msg = DistWheel()
         
     def callback(self, msg):
+        x = 0
+        y = 0
+        theta = 0
+        
         x_value = msg.dist_wheel_left
         y_value = msg.dist_wheel_right
         
+        s_delta = (x_value + y_value)/2
+        theta_delta = (x_value - y_value)/0.1
         
-        self.pub.publish(.25, .25, .8)
+        x_delta = s_delta*cos(theta + theta_delta/2)
+        y_delta = s_delta*sin(theta + theta_delta/2)
+        
+        x_prime = x + x_delta
+        y_prime = y + y_delta
+        theta_prime = theta + theta_delta      
+        
+        self.pub.publish(x_prime, y_prime, theta_prime)
     
         
 if __name__ == '__main__':
