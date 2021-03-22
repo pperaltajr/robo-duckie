749fbcb1a8fa9380e1f6780792e90bea9ae8c593
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-03-22 13:18:29 -0400

    hw7 99% complete

--- packages/image_package/src/image.py
+++ packages/image_package/src/image.py
@@ -16,18 +16,23 @@ class ImageCropper:
         self.bridge = CvBridge()
 
     def callback(self, msg):
-        # convert to a ROS image and crop top half, 
+        # convert to a ROS image and crop top half
         cv_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         crop = np.array(cv_img[240:480, 0:640]) 
         
         # convert to HSV, filter for white and yellow pixels      
         image_hsv = cv2.cvtColor(crop, cv2.COLOR_BGR2HSV)
-        image_white = cv2.inRange(image_hsv, (0,0,215),(150,100,255))
-        image_yellow = cv2.inRange(image_hsv, (0,0,50),(100,100,255))
+        image_white = cv2.inRange(image_hsv, (0,0,215),(190,50,255))
+        image_yellow = cv2.inRange(image_hsv, (0,200,220),(40,255,255))
         
+        # mask
+        output_white = cv2.bitwise_and(crop, crop, mask=image_white)
+        output_yellow = cv2.bitwise_and(crop, crop, mask=image_yellow)
+        
+        # convert back to original
         ros_cropped = self.bridge.cv2_to_imgmsg(crop, "bgr8")
-        ros_white = self.bridge.cv2_to_imgmsg(image_white, "mono8")
-        ros_yellow = self.bridge.cv2_to_imgmsg(image_yellow, "mono8")
+        ros_white = self.bridge.cv2_to_imgmsg(output_white, "bgr8")
+        ros_yellow = self.bridge.cv2_to_imgmsg(output_yellow, "bgr8")
         
         # published cropped, white and yellow images
         self.pub.publish(ros_cropped)
