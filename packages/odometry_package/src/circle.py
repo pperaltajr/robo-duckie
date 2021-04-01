ba3dfa807cea2b55669111455c870ba6dff15ff5
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-04-01 01:25:40 -0400

    lab3update

--- packages/odometry_package/src/circle.py
+++ packages/odometry_package/src/circle.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
         counter = 0    
         while not rospy.is_shutdown():
             counter = counter + 1
-            robot.move(0.3,2)
+            robot.move(0.27,2.3)
             rate.sleep()           
             if counter > 4:
                 robot.move(0,0)
