d74d6e50fa8dd8919e16031d6497e54cc9655c6a
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-04-04 19:13:04 -0400

    hw8 hough issues

--- packages/image_package/src/detect.py
+++ packages/image_package/src/detect.py
@@ -9,42 +9,83 @@ from sensor_msgs.msg import Image
 
 class ImageDetect:
     def __init__(self):
-        rospy.Subscriber('/image_cropped', Image, self.callback)
-        rospy.Subscriber('/image_white', Image, self.callback)
-        rospy.Subscriber('/image_yellow', Image, self.callback)
-        self.pub = rospy.Publisher('/image_canny', Image, queue_size=10)
+        rospy.Subscriber('/image_cropped', Image, self.callback_crop)
+        #rospy.Subscriber('/image_white', Image, self.callback_white)
+        #rospy.Subscriber('/image_yellow', Image, self.callback_yellow)
+        self.canny = rospy.Publisher('/image_canny', Image, queue_size=10)
         self.white = rospy.Publisher('/image_lines_white', Image, queue_size=10)
         self.yellow = rospy.Publisher('/image_lines_yellow', Image, queue_size=10)
+        self.hough = rospy.Publisher('/hough', Image, queue_size=10)
         self.bridge = CvBridge()
 
-    def callback(self, msg):
-        # convert to a ROS image and crop top half
-        cv_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        crop = np.array(cv_img[240:480, 0:640]) 
+    def callback_crop(self, msg):
+        global edge
         
+        # performs canny edge detection on cropped image from hw7
+        cv_img_crop = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        edge = cv2.Canny(cv_img_crop, 120, 255)
+
         # convert to HSV, filter for white and yellow pixels      
-        image_hsv = cv2.cvtColor(crop, cv2.COLOR_BGR2HSV)
+        image_hsv = cv2.cvtColor(cv_img_crop, cv2.COLOR_BGR2HSV)
         image_white = cv2.inRange(image_hsv, (0,0,215),(190,50,255))
         image_yellow = cv2.inRange(image_hsv, (0,60,220),(40,255,255))
         
+        # dilation
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4,4))
+        image_dilate_white = cv2.dilate(image_white, kernel)
+        image_dilate_yellow = cv2.dilate(image_yellow, kernel)
+        
         # mask
-        output_white = cv2.bitwise_and(crop, crop, mask=image_white)
-        output_yellow = cv2.bitwise_and(crop, crop, mask=image_yellow)
+        output_white = cv2.bitwise_and(edge, edge, mask=image_dilate_white)
+        output_yellow = cv2.bitwise_and(edge, edge, mask=image_dilate_yellow)
         
         # convert back to original
-        ros_cropped = self.bridge.cv2_to_imgmsg(crop, "bgr8")
-        ros_white = self.bridge.cv2_to_imgmsg(output_white, "bgr8")
-        ros_yellow = self.bridge.cv2_to_imgmsg(output_yellow, "bgr8")
+        ros_cropped = self.bridge.cv2_to_imgmsg(edge, "mono8")
+        ros_white = self.bridge.cv2_to_imgmsg(output_white, "mono8")
+        ros_yellow = self.bridge.cv2_to_imgmsg(output_yellow, "mono8") 
+
         
         # published cropped, white and yellow images
-        self.pub.publish(ros_cropped)
+        self.canny.publish(ros_cropped)
         self.white.publish(ros_white)
         self.yellow.publish(ros_yellow)
 
+
+        
+        # hough transform
+        global hough_white
+        hough_white = cv2.HoughLinesP(output_white, 1, np.pi/180, 80, 20, 5)
+        hough_yellow = cv2.HoughLinesP(output_yellow, 1, np.pi/180, 80, 20, 5)
+        
+        
+                
+    def output_lines(self, hough_white, hough_white):
+        output = np.copy(hough_white)
+        if hough_white is not None:
+            for i in range(len(hough_white)):
+                l = lines[i][0]
+                cv2.line(output, (l[0],l[1]), (l[2],l[3]), (255,0,0), 2, cv2.LINE_AA)
+                cv2.circle(output, (l[0],l[1]), 2, (0,255,0))
+                cv2.circle(output, (l[2],l[3]), 2, (0,0,255))
+        return output                      
+        
+        self.hough.publish(output_lines)
+        
+    '''     
+    def callback_white(self, msg):
+        cv_img_white = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        combine_white = cv2.bitwise_and(edge, cv_img_white)
+        
+        #convert back to original
+        ros_white = self.bridge.cv2_to_imgmsg(combine_white, "mono8")
+        
+        # published cropped, white and yellow images
+        self.white.publish(ros_white)
+    '''
+    
 if __name__ == '__main__':
     rospy.init_node('detect')
     img_crop = ImageDetect()
     
     #spin() simply keeps python from exiting until this note is stopped   
     rospy.spin()
-    
--- packages/image_package/src/image.py
+++ packages/image_package/src/image.py
@@ -25,9 +25,14 @@ class ImageCropper:
         image_white = cv2.inRange(image_hsv, (0,0,215),(190,50,255))
         image_yellow = cv2.inRange(image_hsv, (0,60,220),(40,255,255))
         
+        # dilation
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4,4))
+        image_dilate_white = cv2.dilate(image_white, kernel)
+        image_dilate_yellow = cv2.dilate(image_yellow, kernel)
+        
         # mask
-        output_white = cv2.bitwise_and(crop, crop, mask=image_white)
-        output_yellow = cv2.bitwise_and(crop, crop, mask=image_yellow)
+        output_white = cv2.bitwise_and(crop, crop, mask=image_dilate_white)
+        output_yellow = cv2.bitwise_and(crop, crop, mask=image_dilate_yellow)
         
         # convert back to original
         ros_cropped = self.bridge.cv2_to_imgmsg(crop, "bgr8")
