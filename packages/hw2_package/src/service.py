18664a35cad6766ae08213aa63e76150e5618db2
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-04-18 01:52:23 -0400

    hw9

--- packages/hw2_package/src/service.py
+++ packages/hw2_package/src/service.py
@@ -1,1 +1,15 @@
-test
+#!/usr/bin/env python3
+
+from beginner_tutorials.srv import AddTwoInts, AddTwoIntsResponse
+import rospy
+
+def hangle_add_two_ints(rep):
+    return AddTwoIntsResponse(req.a +req.b)
+    
+def add_two_ints_server():
+    rospy.init_node('add_two_ints_server')
+    s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
+    
+if __name__ == '__main__':
+    add_two_ints_server()
+    rospy.spin()
