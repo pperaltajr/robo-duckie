35817804073632ae41da93fba327102e8a0cb7ea
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-04-18 23:04:40 -0400

    hw9

--- packages/hw2_package/src/service.py
+++ packages/hw2_package/src/service.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
-from beginner_tutorials.srv import AddTwoInts, AddTwoIntsResponse
 import rospy
+from beginner_tutorials.srv import*
+    
+def add_two_ints_client(x,y):
+    rospy.wait_for_service('add_two_ints')
+    try:
+        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
+        resp1 = add_two_ints(x,y)
+        return resp1.sum
+    except rospy.ServiceException, e:
+        print("Service call failed: %s" %e)
 
-def hangle_add_two_ints(rep):
-    return AddTwoIntsResponse(req.a +req.b)
-    
-def add_two_ints_server():
-    rospy.init_node('add_two_ints_server')
-    s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
-    
+
 if __name__ == '__main__':
-    add_two_ints_server()
-    rospy.spin()
+    add_two_ints_client(1,2)
