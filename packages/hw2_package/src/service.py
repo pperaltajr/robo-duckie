4d9c9b60c10a78d294f3056c739ba2870e42f6cb
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-04-27 20:04:30 -0400

    hw9

--- packages/hw2_package/src/service.py
+++ packages/hw2_package/src/service.py
@@ -1,45 +1,59 @@
 #! /usr/bin/env python3
 
+import time
 import rospy
-
-# Brings in the SimpleActionClient
 import actionlib
-
-# Brings in the messages used by the fibonacci action, including the
-# goal message and the result message.
 import example_action_server.msg
+from std_msgs.msg import Float32
+from example_service.srv import Fibonacci
 
-def fibonacci_client():
-    # Creates the SimpleActionClient, passing the type of the action
-    # (FibonacciAction) to the constructor.
-    print("starting client")
-    client = actionlib.SimpleActionClient('fibonacci', example_action_server.msg.FibonacciAction)
-
-    print("waiting for server")
-    # Waits until the action server has started up and started
-    # listening for goals.
+def fibonacci_client(num):
+    client = actionlib.SimpleActionClient('/fibonacci', example_action_server.msg.FibonacciAction)
+    
+    rospy.loginfo("waiting for server")
     client.wait_for_server()
     
-    print("sending goal")
-    # Creates a goal to send to the action server.
-    goal = example_action_server.msg.FibonacciGoal(order=10)
-
-    # Sends the goal to the action server.
+    rospy.loginfo("sending goal")
+    goal = example_action_server.msg.FibonacciGoal(order=num)
     client.send_goal(goal)
-
-    print("waiting for result")
-    # Waits for the server to finish performing the action.
+    rospy.loginfo("goal sent")
+    
     client.wait_for_result()
-
-    # Prints out the result of executing the action
-    return client.get_result()  # A FibonacciResult
+    rsopy.loginfo("goal received")
+    
+    return client.get_result()
+    
+def fibcalc(num)
+    rospy.loginfo("waiting for service")
+    rospy.wait_for_service("/calc_fibonacci")
+    try:
+        fibcalc = rospy.ServiceProxy("/calc_fibonacci", Fibonacci)
+        rospy.loginfo("service requested")
+        answer = fibcalc(num)
+        rospy.loginfo("result received")
+        print(answer)
+        return answer
+        
+    except rospy.ServiceException as exc:
+        print("Service did not process request: " + str(exc))
+        rospy.loginfo("Service Error")
+        
+    
+ 
 
 if __name__ == '__main__':
     try:
         # Initializes a rospy node so that the SimpleActionClient can
         # publish and subscribe over ROS.
+        time.sleep(3)
         rospy.init_node('fibonacci_client_py')
-        result = fibonacci_client()
+        fibcalc(3)
+        result = fibonacci_client(3)
         print("Result:", ', '.join([str(n) for n in result.sequence]))
+        
+        fibcalc(15)
+        result = fibonacci_client(15)
+        print("Result:", ', '.join([str(n) for n in result.sequence]))
+        
     except rospy.ROSInterruptException:
         print("program interrupted before completion", file=sys.stderr)
