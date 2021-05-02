82576eaa685bd45a7c70cfde2c1b0ead00d48d1c
Pedro Peralta Jr <pedro_peralta@student.uml.edu>
2021-05-01 21:01:54 -0400

    hw10

--- packages/pid_package/src/pid.py
+++ packages/pid_package/src/pid.py
@@ -7,18 +7,18 @@ from pid_controller import PidController
 
 class PID:
     def __init__(self):
-    rospy.Subscriber("/error", Float32, self.callback)
-    self.pub_control = rospy.Publisher("control_input", Float32, queue_size=10)
-    self.controller = PidController()
-    self.controller.Gains(0.16, 0.2, 0.8)
+        rospy.Subscriber("/error", Float32, self.callback)
+        self.pub_control = rospy.Publisher("control_input", Float32, queue_size=10)
+        self.controller = PidController()
+        self.controller.Gains(.108, .01, .8)
     
-    def callback(self, data)
+    def callback(self, data):
         error = data.data
-        system_control = self.controller.calculate(error, time.time())
-        self.pub.publish(system_control)
+        system_control = self.controller.Calculations(error, time.time())
+        self.pub_control.publish(system_control)
         
-if __name__ == '__main__'
+if __name__ == '__main__':
     rospy.init_node('pid')
     PID()
     rospy.set_param("/controller_ready", "true")
-    ropsy.spin
+    rospy.spin()
--- packages/pid_package/src/pid_controller.py
+++ packages/pid_package/src/pid_controller.py
@@ -3,14 +3,14 @@
 import time
 import math
 
-class PidController
+class PidController:
     def __init__(self):
         self.last_time = time.time() 
-        self.last_integral = ()
-        self.last_error = ()        
-        self.kp = ()
-        self.ki = ()
-        self.kd = ()
+        self.last_integral = 0
+        self.last_error = 0       
+        self.kp = 0
+        self.ki = 0
+        self.kd = 0
     
     def Gains(self, p_kp, p_ki, p_kd):
         self.kp = p_kp
@@ -19,9 +19,9 @@ class PidController
         
     def Calculations(self, p_error, p_time):
         dt = p_time - self.last_time
-        integral = self.last_integral + (p_error*dt)
+        integral = self.last_integral + p_error*dt
         derivative = (p_error - self.last_error) / dt
-        control = self.kp*p_error + self.kin*integral + self.kd*derivative)
+        control = self.kp*p_error + self.ki*integral + self.kd*derivative
         
         self.last_error = p_error
         self.last_time = p_time
